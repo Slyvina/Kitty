5e293b8f8794478369994f233f163ec2d026aa4b
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-05 18:20:46 +0200

    Why all those needless fucking whitelines?

--- Source/KittyHighC.cpp
+++ Source/KittyHighC.cpp
@@ -17,14 +17,12 @@
 // misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 // EndLic
+
 #include <Kitty_High.hpp>
 
 namespace Slyvina {
-
 	namespace Kitty {
-
 		void KittyHighC() {
-
 			static _KittyPL Me{};
 
 		
@@ -32,149 +30,77 @@ namespace Slyvina {
 
 
 			_KittyHigh::Langs["c"] = Me.Base;
-
-			_KittyHigh::Langs["h"] = Me.Base;
-
-            _KittyHigh::Langs["cpp"] = Me.Base;
-
-            _KittyHigh::Langs["hpp"] = Me.Base;
-
-			Me.Base->Language = "C/C++";
-
-            auto
-
-                BaseTypes{ Me.BaseTypes },
-
-                KeyWords{ Me.KeyWords };
-
-            KeyWords->push_back("void");
-
-            BaseTypes->push_back("bool");
-
-            BaseTypes->push_back("auto");
-
-            BaseTypes->push_back("char");
-
-            BaseTypes->push_back("int");
-
-            KeyWords->push_back("asm");
-
-            KeyWords->push_back("else");
-
-            KeyWords->push_back("new");
-
-            KeyWords->push_back("this");
-
-            KeyWords->push_back("enum");
-
-            KeyWords->push_back("operator");
-
-            KeyWords->push_back("throw");
-
-            KeyWords->push_back("explicit");
-
-            KeyWords->push_back("private");
-
-            KeyWords->push_back("true");
-
-            KeyWords->push_back("break");
-
-            KeyWords->push_back("export");
-
-            KeyWords->push_back("protected");
-
-            KeyWords->push_back("try");
-
-            KeyWords->push_back("case");
-
-            KeyWords->push_back("extern");
-
-            KeyWords->push_back("public");
-
-            KeyWords->push_back("typedef");
-
-            KeyWords->push_back("catch");
-
-            KeyWords->push_back("false");
-
-            KeyWords->push_back("register");
-
-            KeyWords->push_back("typeid");
-
-            KeyWords->push_back("float");
-
-            KeyWords->push_back("reinterpret_cast");
-
-            KeyWords->push_back("typename");
-
-            KeyWords->push_back("class");
-
-            KeyWords->push_back("for");
-
-            KeyWords->push_back("return");
-
-            KeyWords->push_back("union");
-
-            KeyWords->push_back("const");
-
-            KeyWords->push_back("friend");
-
-            KeyWords->push_back("short");
-
-            KeyWords->push_back("unsigned");
-
-            KeyWords->push_back("const_cast");
-
-            KeyWords->push_back("goto");
-
-            KeyWords->push_back("signed");
-
-            KeyWords->push_back("using");
-
-            KeyWords->push_back("continue");
-
-            KeyWords->push_back("if");
-
-            KeyWords->push_back("sizeof");
-
-            KeyWords->push_back("virtual");
-
-            KeyWords->push_back("default");
-
-            KeyWords->push_back("inline");
-
-            KeyWords->push_back("static");
-
-            KeyWords->push_back("delete");
-
-            KeyWords->push_back("static_cast");
-
-            KeyWords->push_back("volatile");
-
-            KeyWords->push_back("do");
-
-            KeyWords->push_back("long");
-
-            KeyWords->push_back("struct");
-
-            KeyWords->push_back("wchar_t");
-
-            KeyWords->push_back("double");
-
-            KeyWords->push_back("mutable");
-
-            KeyWords->push_back("switch");
-
-            KeyWords->push_back("while");
-
-            KeyWords->push_back("dynamic_cast");
-
-            KeyWords->push_back("namespace");
-
-            KeyWords->push_back("template");
-
-
-
+						_KittyHigh::Langs["h"] = Me.Base;
+									_KittyHigh::Langs["cpp"] = Me.Base;
+												_KittyHigh::Langs["hpp"] = Me.Base;
+															Me.Base->Language = "C/C++";
+
+			auto
+				BaseTypes{ Me.BaseTypes },
+				KeyWords{ Me.KeyWords };
+			KeyWords->push_back("void");
+			BaseTypes->push_back("bool");
+			BaseTypes->push_back("auto");
+			BaseTypes->push_back("char");
+			BaseTypes->push_back("int");
+			KeyWords->push_back("asm");
+			KeyWords->push_back("else");
+			KeyWords->push_back("new");
+			KeyWords->push_back("this");
+			KeyWords->push_back("enum");
+			KeyWords->push_back("operator");
+			KeyWords->push_back("throw");
+			KeyWords->push_back("explicit");
+			KeyWords->push_back("private");
+			KeyWords->push_back("true");
+			KeyWords->push_back("break");
+			KeyWords->push_back("export");
+			KeyWords->push_back("protected");
+			KeyWords->push_back("try");
+			KeyWords->push_back("case");
+			KeyWords->push_back("extern");
+			KeyWords->push_back("public");
+			KeyWords->push_back("typedef");
+			KeyWords->push_back("catch");
+			KeyWords->push_back("false");
+			KeyWords->push_back("register");
+			KeyWords->push_back("typeid");
+			KeyWords->push_back("float");
+			KeyWords->push_back("reinterpret_cast");
+			KeyWords->push_back("typename");
+			KeyWords->push_back("class");
+			KeyWords->push_back("for");
+			KeyWords->push_back("return");
+			KeyWords->push_back("union");
+			KeyWords->push_back("const");
+			KeyWords->push_back("friend");
+			KeyWords->push_back("short");
+			KeyWords->push_back("unsigned");
+			KeyWords->push_back("const_cast");
+			KeyWords->push_back("goto");
+			KeyWords->push_back("signed");
+			KeyWords->push_back("using");
+			KeyWords->push_back("continue");
+			KeyWords->push_back("if");
+			KeyWords->push_back("sizeof");
+			KeyWords->push_back("virtual");
+			KeyWords->push_back("default");
+			KeyWords->push_back("inline");
+			KeyWords->push_back("static");
+			KeyWords->push_back("delete");
+			KeyWords->push_back("static_cast");
+			KeyWords->push_back("volatile");
+			KeyWords->push_back("do");
+			KeyWords->push_back("long");
+			KeyWords->push_back("struct");
+			KeyWords->push_back("wchar_t");
+			KeyWords->push_back("double");
+			KeyWords->push_back("mutable");
+			KeyWords->push_back("switch");
+			KeyWords->push_back("while");
+			KeyWords->push_back("dynamic_cast");
+			KeyWords->push_back("namespace");
+			KeyWords->push_back("template");
 		}
 
 	}
