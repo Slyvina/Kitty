43936d9d25ad1c34a6c97af486ce4d229ed89f6c
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-05 15:48:36 +0200

    WIP

---
+++ Headers/Kitty_High.hpp
@@ -0,0 +1,291 @@
+// Lic:
+// Kitty/Headers/Kitty_High.hpp
+// Kitty High (header)
+// version: 24.10.05
+// Copyright (C) 2019, 2020, 2023 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+
+#pragma once
+
+#include <SlyvQCol.hpp>
+
+namespace Slyvina {
+    namespace Kitty {
+
+#pragma region "Class to shared pointer.
+        // Remember this is just manually translated C# code, and C# only uses pointers for classes and has automated garbage collection.
+        // C++ has none of that.
+#define KittyClass(real,pseudo) class real; typedef std::shared_ptr<real> pseudo;
+        KittyClass(_KittyOutput, KittyOutput);
+        KittyClass(_KittyHigh, KittyHigh);
+        class _KittyPL;
+
+
+
+        typedef void (*KittyOutputVoidString)(_KittyOutput*, std::string);
+        typedef void (*KittyOutputShow)(_KittyHigh* SELF,std::string src, bool linenumbers);
+#pragma region
+
+
+        struct _KittyColors {
+            Units::qColor
+                KeyWord = Units::qColor::Yellow,
+                Identifier = Units::qColor::White,
+                Number = Units::qColor::Cyan,
+                String = Units::qColor::Green,
+                Comment = Units::qColor::DarkGrey,
+                LineNumbers = Units::qColor::DarkGrey,
+                Other = Units::qColor::Grey,
+                Error = Units::qColor::Red,
+                Attribute = Units::qColor::Blue,
+                BaseType = Units::qColor::Magenta;
+        };
+        _KittyColors KittyColors;
+
+        
+        class _KittyOutput {
+        public:
+            ~_KittyOutput() {
+                //Console.ResetColor();
+                QCol->Reset();
+            }
+            /* Due to bad experiences with abstract methods (or virutals as they are called in C++), I'll do this a different way
+            abstract public void Write(string a);
+            abstract public void WriteLine(string a);
+            */
+            KittyOutputVoidString abstract_Write{ nullptr };
+            KittyOutputVoidString abstract_WriteLine{ nullptr };
+            inline void Write(std::string a) { if (!abstract_Write) throw std::runtime_error("abstract_Write not set!"); abstract_Write(this, a); }
+            inline void WriteLine(std::string a) { if (!abstract_WriteLine) throw std::runtime_error("abstract_Write not set!"); abstract_WriteLine(this, a); }
+            inline void WriteLine() { WriteLine(""); }
+            inline void Write(char c) { char ca[2]{ c,0 }; Write(ca); } // Dirty, but what works that works!
+            /* Due to lack of a proper cross platform way to do this in C++ , this will be kept on hold.
+            public ConsoleKeyInfo ReadKey() = > Console.ReadKey();
+            public ConsoleKeyInfo ReadKey(bool b) = > Console.ReadKey(b);
+            */
+            void ResetColor();
+            // I don't expect to need this, and if I do, I might to go a different approach as this is too reliant on C#'s approach
+            //public void Write(object a) = > Write($"{a}");
+            // public void WriteLine(object a) = > WriteLine($"{a}");
+            Units::qColor ForegroundColor{ Units::qColor::Grey };
+            Units::qColor BackgroundColor{ Units::qColor::Black };
+            int WindowHeight = 25;
+        };
+
+        //class KittyCLI :KittyOutput {
+        KittyOutput KittyCLI();
+        
+
+        class _KittyHigh {
+        private:
+            static KittyOutput _ko;
+            //String _Language{ "" };
+            //inline void Language(String L) { _Language = L; }
+        public:
+            static std::map<String, KittyHigh> Langs; //static readonly public SortedDictionary<string, KittyHigh> Langs = new SortedDictionary<string, KittyHigh>();
+
+            static int NumLines();
+            static int PagLines;
+            static bool BrkLines;
+            static KittyOutput Console();
+            static void Console(KittyOutput ko);
+
+
+            static void PageBreak();
+
+            inline static void WriteLine(String a = "") {
+                Console()->WriteLine(a);
+                PageBreak();
+            }
+
+            //public abstract void Show(string src, bool linenumbers = false);
+            KittyOutputShow abstract_Show{ nullptr };
+            void Show(String src, bool linenumbers = false);
+            //public void Show(StringBuilder src, bool linenumbers = false) = > Show(src, linenumbers);
+            String Language{ "" }; //inline String Language() { return _Language; }
+            static void LineNumber(int ln);
+
+            static void Init();
+            _KittyPL* PLKid{ nullptr };
+        };
+
+
+        class _KittyPL { //: KittyHigh {
+        public:
+            KittyHigh Base{ std::shared_ptr<_KittyHigh>(new _KittyHigh()) };
+            VecString KeyWords = NewVecString(); //protected List<string> KeyWords = new List<string>();
+            VecString BaseTypes = NewVecString(); //List<string> BaseTypes = new List<string>();
+            String stringstart = "\"";
+            String stringend = "\"";
+            String mlstringstart = "@\"";
+            String mlstringend = "\"";
+            String astringstart = "'";
+            String astringend = "'";
+            String singcomment = "//";
+            String mulcommentstart = "/*";
+            String mulcommentend = "*/";
+            char escape = '\\';
+            bool mulcomment = true;
+            bool mulcommentfullline = false; // Needed for BlitzMax where commands taking up a full line are used for multi line comments. This is not entirely fool-proof, but it'll have to do.
+            bool caseinsensitive = false;
+            bool supcom = true;
+
+            _KittyPL();
+
+        };
+
+        class KittyNiks : KittyHigh {
+            public KittyNiks() { Language = "Unrecognized"; }
+            public override void Show(string src, bool linenumbers = false) {
+                src = src.Replace("\r\n", "\n");
+                var lines = src.Split('\n');
+                for (int i = 0; i < lines.Length; i++) {
+                    if (linenumbers) LineNumber(i + 1);
+                    Console.ForegroundColor = KittyColors.Other; WriteLine(lines[i]);
+                }
+            }
+        }
+
+        // Markup support by freezernick
+        class KittyMarkup : KittyHigh
+        {
+            protected string stringstart = "\"";
+            protected string stringend = "\"";
+            protected bool mulcomment = true;
+            protected string mulcommentstart = "<!--";
+            protected string mulcommentend = "-->";
+            protected string opentagchar = "<";
+            protected string closetagchar = ">";
+            protected string endtagchar = "/>";
+            protected char escape = '\\';
+
+            
+            public override void Show(string src, bool linenumbers = false) {
+                bool intag = false;
+                string word = "";
+                void showword() {
+                    var col = KittyColors.Other;
+                    if (word.StartsWith(opentagchar) || word.EndsWith(endtagchar) || word.EndsWith(closetagchar))
+                        col = KittyColors.KeyWord;
+                    if (intag)
+                        col = KittyColors.Attribute;
+                    Console.ForegroundColor = col;
+                    Console.Write(word);
+                }
+                src = src.Replace("\r\n", "\n");
+                var lines = src.Split('\n');
+                bool mulcomm = false;
+                for (int i = 0; i < lines.Length; i++) {
+                    if (linenumbers) LineNumber(i + 1);
+                    word = "";
+                    intag = false;
+                    var singstring = false;
+                    var stringescape = false;
+                    bool wassingstring;
+
+                    for (int p = 0; p < lines[i].Length; p++) {
+                        var ch = lines[i][p];
+
+                        wassingstring = singstring;
+                        singstring = singstring || (p < lines[i].Length - 1 && lines[i].Substring(p, stringstart.Length) == stringstart);
+                        mulcomm = mulcomm || (p < lines[i].Length - 3 && lines[i].Substring(p, mulcommentstart.Length) == mulcommentstart && !singstring && mulcomment);
+                        if (singstring) {
+                            Console.ForegroundColor = KittyColors.String;
+                            Console.Write($"{ch}");
+                            if (wassingstring && p < lines[i].Length && lines[i].Substring(p, stringend.Length) == stringend && !stringescape)
+                                singstring = false;
+                            else
+                                stringescape = ch == escape && !stringescape;
+                        } else if (mulcomm) {
+                            Console.ForegroundColor = KittyColors.Comment;
+                            Console.Write($"{ch}");
+                            if (p < lines[i].Length - 2 && lines[i].Substring(p, mulcommentend.Length) == mulcommentend) {
+                                Console.Write($"{lines[i][p + 1]}");
+                                Console.Write($"{lines[i][p + 2]}");
+                                p = p + 2;
+                                mulcomm = false;
+                            }
+                        } else if (word == "" && ch == '<') {
+                            word += $"{ch}";
+                            int q = p;
+                            bool inline = true;
+                            while (inline) {
+                                q++;
+                                if (lines[i][q] == '=') {
+                                    word += lines[i][q];
+                                    p = q;
+                                    showword();
+                                    word = "";
+                                    intag = true;
+                                    inline = false;
+                                } else if (lines[i][q] == '>') {
+                                    word += lines[i][q];
+                                    p = q;
+                                    inline = false;
+                                    intag = false;
+                                    showword();
+                                    word = "";
+                                } else if (lines[i][q] == ' ') {
+                                    word += lines[i][q];
+                                    p = q;
+                                    showword();
+                                    word = "";
+                                    inline = false;
+                                    intag = true;
+                                } else {
+                                    word += lines[i][q];
+                                }
+                            }
+                        } else if (word == "" && intag && ch != ' ' && ch != '>' && ch != '/') {
+                            word += $"{ch}";
+                            int q = p;
+                            bool inline = true;
+                            while (inline) {
+                                q++;
+                                if (lines[i][q] == '=') {
+                                    word += lines[i][q];
+                                    p = q;
+                                    showword();
+                                    word = "";
+                                    inline = false;
+                                } else if (lines[i][q] == '>') {
+                                    word += lines[i][q];
+                                    p = q;
+                                    inline = false;
+                                    intag = false;
+                                } else {
+                                    word += lines[i][q];
+                                }
+                            }
+                        } else if (word == "" && intag && ch == '/' && lines[i][p + 1] == '>') {
+                            p += 1;
+                            word = "/>";
+                            intag = false;
+                            showword();
+                            word = "";
+                        } else {
+                            if (ch == '>')
+                                intag = false;
+                            if (word != "") showword(); word = $"{ch}"; showword(); word = "";
+                        }
+                    }
+                    if (word != "") showword();
+                    WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Source/Kitty_High.cpp
@@ -0,0 +1,275 @@
+// Lic:
+// Kitty/Source/Kitty_High.cpp
+// Kitty High
+// version: 24.10.05
+// Copyright (C) 2019, 2020, 2023 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+#include "../Headers/Kitty_High.hpp"
+#include <SlyvSilly.hpp>
+#include <SlyvString.hpp>
+
+using namespace Slyvina::Units;
+
+
+
+namespace Slyvina {
+	namespace Kitty {
+
+		void _KittyOutput::ResetColor() {
+			 ForegroundColor = qColor::Grey; 
+			 BackgroundColor = qColor::Black; 
+		}
+
+#pragma region KittyCLI
+		static void KittyCLI_Write(_KittyOutput* SELF, String a) {
+			QCol->Color(
+				SELF->ForegroundColor,
+				SELF->BackgroundColor);
+			printf("%s", a.c_str());
+		}
+		static void KittyCLI_WriteLine(_KittyOutput* SELF, String a) { KittyCLI_Write(SELF, a + "\n"); }
+		KittyOutput KittyCLI() {
+			//WindowHeight = Console.WindowHeight;
+			auto ret{ new _KittyOutput() };
+			ret->WindowHeight = 25; // I don't think there's a cross-platform way to do the auto check anyway, but who knows!
+			ret->abstract_Write = KittyCLI_Write;
+			ret->abstract_WriteLine = KittyCLI_WriteLine;
+			return std::shared_ptr<_KittyOutput>(ret);
+		}
+#pragma endregion
+
+#pragma region KittyHigh
+		std::map<String, KittyHigh> _KittyHigh::Langs{};
+		KittyOutput _KittyHigh::_ko{ nullptr };
+		int _KittyHigh::NumLines() { return Console()->WindowHeight; }
+		KittyOutput _KittyHigh::Console() {
+			if (_ko == nullptr) _ko = KittyCLI();
+			return _ko;
+		}
+
+		void _KittyHigh::Console(KittyOutput value) {
+			//System.Diagnostics.Debug.WriteLine("New Console For KittyHigh set");
+			_ko = value;
+		}
+
+		void _KittyHigh::PageBreak() {
+			if (!BrkLines) return;
+			PagLines++;
+			if (PagLines >= NumLines() - 2) {
+				/* Due to C++ not having a good cross-platform equivalent to C#'s Console.ReadKey, I will (for now) dummy this code.
+				Console.Write(" ");
+				Console.ForegroundColor = ConsoleColor.Blue;
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.Write(" More ");
+				Console.ResetColor();
+				Console.Write("\b\b\b\b\b\b\b");
+				ConsoleKeyInfo k;
+				bool loopme = true;
+				do {
+					loopme = false;
+					k = Console.ReadKey(true);
+					Console.Write("       ");
+					Console.Write("\b\b\b\b\b\b\b");
+					switch (k.Key) {
+					case ConsoleKey.Enter:
+						PagLines = 0;
+						break;
+					case ConsoleKey.Spacebar:
+						PagLines--;
+						break;
+					case ConsoleKey.Escape:
+						BrkLines = false;
+						break;
+					default:
+						loopme = true;
+						break;
+					}
+				} while (loopme);
+				*/
+				pause(); // This will have to do the trick (for now)
+			}
+		}
+
+		void _KittyHigh::Show(String src, bool linenumbers) {
+			if (!abstract_Show) {
+				throw std::runtime_error("Now show function set for highlighting language \""+Language()+"\"");
+				return;
+			}
+			abstract_Show(this, src, linenumbers);
+		}
+
+		void _KittyHigh::LineNumber(int ln) {
+			Console()->ForegroundColor = KittyColors.LineNumbers;
+			//Console.Write($"{ln.ToString("D9")} | ");
+			Console()->Write(TrSPrintF("%9d |", ln));
+		}
+
+		void _KittyHigh::Init() { Langs["OTHER"] = KittyNiks(); }
+
+		int _KittyHigh::PagLines{ 0 };
+		bool _KittyHigh::BrkLines{ false };
+#pragma endregion
+
+#pragma region "PL (Programming Language)"
+
+		// Disgusting, but it helps with this C# -> C++ problem.
+#define showword() __showword(SELF->PLKid,word,mle)
+		static void __showword(_KittyPL* PL, String& word, int& mle) {
+				//static String word{ "" };
+				//static int mle{ 0 };
+				//if (resetshowword) { word = ""; mle = 0; return; }
+				auto col = KittyColors.Other;
+				auto KeyWords{ PL->KeyWords };
+				auto BaseTypes{ PL->BaseTypes };
+				auto caseinsensitive{ PL->caseinsensitive };
+				auto Console{ PL->Base->Console() };
+				if (VecHasString( KeyWords,word) || (caseinsensitive && VecHasString(KeyWords,Lower(word)) ))
+					col = KittyColors.KeyWord;
+				else if (VecHasString(BaseTypes,word) || (caseinsensitive && VecHasString(BaseTypes,Lower(word))))
+					col = KittyColors.BaseType;
+				else {
+					// Not the most elegant method, but what works, that works!
+					
+					//switch (word.Substring(0, 1).ToUpper()) { 
+					auto uword{ Upper(word) }; 
+					switch( uword[0] ) {                    
+					case 'A':
+					case 'B':
+					case 'C':
+					case 'D':
+					case 'E':
+					case 'F':
+					case 'G':
+					case 'H':
+					case 'I':
+					case 'J':
+					case 'K':
+					case 'L':
+					case 'M':
+					case 'N':
+					case 'O':
+					case 'P':
+					case 'Q':
+					case 'R':
+					case 'S':
+					case 'T':
+					case 'U':
+					case 'V':
+					case 'W':
+					case 'X':
+					case 'Y':
+					case 'Z':
+					case '_':
+						col = KittyColors.Identifier;
+						break;
+					case '0':
+					case '1':
+					case '2':
+					case '3':
+					case '4':
+					case '5':
+					case '6':
+					case '7':
+					case '8':
+					case '9':
+						col = KittyColors.Number;
+						break;
+					}
+				}
+				Console->ForegroundColor = col;
+				Console->Write(word);
+			}
+		static void _PLShow(_KittyHigh* SELF, String src, bool linenumbers = false) {
+			//showword(SELF->PLKid,true); // Reset
+			String word{ "" };
+			int mle{ 0 };
+			src = StReplace(src, "\r\n", "\n");
+			auto lines{ Split(src,'\n') }; //= src.Split('\n');
+			auto mulcomm = false;
+			auto Console{ SELF->Console() };
+			// Not supporting abstracts has its downsizes, but we'll manage!
+			auto& singcomment{ SELF->PLKid->singcomment };
+			auto& mulcommentstart{ SELF->PLKid -> mulcommentstart };
+			auto& mulcomment{ SELF->PLKid->mulcomment };
+			auto& stringstart{ SELF->PLKid->stringstart };
+			auto& escape{ SELF->PLKid->escape };
+			auto& mulcommentend{ SELF->PLKid->mulcommentend };
+			for (int i = 0; i < lines->size(); i++) {
+				if (linenumbers) SELF->LineNumber(i + 1);
+				word = "";
+				auto singcomm = false;
+				auto singstring = false;
+				auto stringescape = false;
+				auto wassingstring = false;
+				for (int p = 0; p < (*lines)[i].size(); p++) {
+					auto ch = (*lines)[i][p];
+					// Console.WriteLine($"DEBUG! {(*lines)[i].Substring(p, singcomment.Length)} {singcomment}");
+					wassingstring = singstring;
+					singcomm = singcomm || (p < (*lines)[i].size() - 1 && (*lines)[i].substr(p, singcomment.size()) == singcomment && (!singstring) && (!mulcomm) && supcom);
+					try {
+						mulcomm = mulcomm || (p < (*lines)[i].size() - 1 && (*lines)[i].substr(p, std::min((*lines)[i].size() - p, mulcommentstart.size())) == mulcommentstart && (!singstring) & (!singcomm) && mulcomment);
+					} catch (std::runtime_error e) {
+#ifndef NDEBUG
+						//System.Diagnostics.Debug.WriteLine($"Error: {e.Message} (parsing line {i+1}/{lines.size()}; pos {p})");
+						QCol->Error(String(e.what()) + TrSPrintF(" (parsing line %d/%d; pos %s", i + 1, lines->size(), p));
+#endif
+						//System.Console.Beep(); mle++;
+						//mulcomm = false;
+						Beep();
+					}
+					singstring = singstring || (p < (*lines)[i].size() - 1 && (*lines)[i].substr(p, stringstart.size()) == stringstart && (!singcomm) && (!mulcomm));
+					if (singstring) {
+						Console->ForegroundColor = KittyColors.String;
+						Console->Write(TrSPrintF("%c",ch));
+						if (wassingstring && p < (*lines)[i].size() && (*lines)[i].Substring(p, stringend.size()) == stringend && !stringescape)
+							singstring = false;
+						else
+							stringescape = ch == escape && !stringescape;
+					} else if (mulcomm) {
+						Console->ForegroundColor = KittyColors.Comment;
+						Console->Write(ch);
+						if (p < (*lines)[i].size() - (mulcommentend.size() - 1) && (*lines)[i].Substring(p, mulcommentend.size()) == mulcommentend)
+							mulcomm = false;
+					} else if (singcomm) {
+						Console->ForegroundColor = KittyColors.Comment;
+						Console->Write(ch);
+					} else {
+						if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '_' || (ch >= '0' && ch <= '9')) {
+							//word += $"{ch}";							
+							word += ch;
+						} else {
+							char chnw[2]{ ch,0 };
+							if (word != "") showword(); word = chnw; showword(); word = "";
+						}
+					}
+				}
+				if (word != "") showword();
+				SELF->WriteLine();
+			}
+#ifndef NDEBUG
+			if (mle > 0)
+				printf("Number of errors on multi-line-comments: %d", mle);
+				//System.Diagnostics.Debug.WriteLine($"Number of errors on multi-line-comments: {mle}");
+#endif
+		}
+
+
+		_KittyPL::_KittyPL() {
+			Base->PLKid = this;
+			Base->abstract_Show = _PLShow;
+		}
+	}
+}
