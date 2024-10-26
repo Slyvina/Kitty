f3f24409909e589b3717dbfce156aa1b8b9513e7
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-26 14:16:05 +0200

    Recognize: - SConstruct => Python - SVG => XML

--- Source/KittyHighXML.cpp
+++ Source/KittyHighXML.cpp
@@ -1,3 +1,26 @@
+// License:
+// 	Kitty/Source/KittyHighXML.cpp
+// 	Kitty High eXtensible Markup Language
+// 	version: 24.10.26
+// 
+// 	Copyright (C) 2019, 2024 Freezernick
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
 // Lic:
 // Kitty/Source/KittyHighXML.cpp
 // Kitty High eXtensible Markup Language
@@ -27,6 +50,7 @@ namespace Slyvina {
 			B->Langs["csproj"] = B;
 			B->Langs["vcxproj"] = B;
 			B->Langs["xaml"] = B;
+			B->Langs["svg"] = B;
 			B->Language = "eXtensible Markup Language";
 			Me.stringstart = "\"";
 			Me.stringend = "\"";
