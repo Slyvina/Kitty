c2d732c1e9d428065677527f5669141df408a09f
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-12-31 19:25:18 +0100

    Dupe license removal

--- Source/KittyHighNIL.cpp
+++ Source/KittyHighNIL.cpp
@@ -1,7 +1,7 @@
 // License:
 // 	Kitty/Source/KittyHighNIL.cpp
 // 	Kitty High NIL Isn't Lua
-// 	version: 24.11.16
+// 	version: 24.12.31
 // 
 // 	Copyright (C) 2019, 2020, 2023, 2024 Jeroen P. Broks
 // 
@@ -21,25 +21,6 @@
 // 	   misrepresented as being the original software.
 // 	3. This notice may not be removed or altered from any source distribution.
 // End License
-// Lic:
-// Kitty/Source/KittyHighNIL.cpp
-// Kitty High NIL Isn't Lua
-// version: 24.10.05
-// Copyright (C) 2019, 2020, 2023, 2024 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
 
 #include <Kitty_High.hpp>
 
