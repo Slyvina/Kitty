3f18cb225ea0c9a5655f9a9a422ac269a0e977d9
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2025-01-06 22:13:11 +0100

    Update KittyHighScyndi.cpp

--- Source/KittyHighScyndi.cpp
+++ Source/KittyHighScyndi.cpp
@@ -1,9 +1,9 @@
 // License:
 // 	Kitty/Source/KittyHighScyndi.cpp
 // 	Kitty High Scyndi Programming Language
-// 	version: 24.11.16
+// 	version: 25.01.06
 // 
-// 	Copyright (C) 2019, 2020, 2023, 2024 Jeroen P. Broks
+// 	Copyright (C) 2019, 2020, 2023, 2024, 2025 Jeroen P. Broks
 // 
 // 	This software is provided 'as-is', without any express or implied
 // 	warranty.  In no event will the authors be held liable for any damages
@@ -21,25 +21,7 @@
 // 	   misrepresented as being the original software.
 // 	3. This notice may not be removed or altered from any source distribution.
 // End License
-// Lic:
-// Kitty/Source/KittyHighScyndi.cpp
-// Kitty High Scyndi Programming Language
-// version: 24.10.05
-// Copyright (C) 2019, 2020, 2023, 2024 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
+
 #include <Kitty_High.hpp>
 
 namespace Slyvina {
@@ -121,7 +103,7 @@ namespace Slyvina {
 
             KeyWords->push_back("module");
 
-
+            KeyWords->push_back("deftable");
 
         }
     }
